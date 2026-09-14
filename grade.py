6e4713194c39f5ae2cb032be24532267430b9818
La Min Thant <139468380+Lamin-Thant@users.noreply.github.com>
2026-09-14 13:20:26 +0630

    Add pass or fail result

--- grade.py
+++ grade.py
@@ -5,5 +5,11 @@ second_mark = float(input("Enter second subject mark: "))
 
 average_mark = (first_mark + second_mark) / 2
 
+if average_mark >= 50:
+    result = "Pass"
+else:
+    result = "Fail"
+
 print("\nStudent Name:", student_name)
 print("Average Mark:", average_mark)
+print("Result:", result)
